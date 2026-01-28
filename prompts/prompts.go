cb7b28c6a0c6745ec7bdb7b4092f1db6b7346902
jiro4989 <jiroron666@gmail.com>
2026-01-28 22:31:57 +0900

    fix: パブリック関数や型をプライベートに変更

--- prompts/prompts.go
+++ prompts/prompts.go
@@ -4,35 +4,35 @@ import (
 	"math/rand"
 )
 
-type PromptFunc func(string) (bool, error)
+type promptFunc func(string) (bool, error)
 
-type Prompts map[string]PromptFunc
+type promptFuncs map[string]promptFunc
 
 const appname = "tene3rm"
 
 // Prompt はランダムにプロンプトを表示し、ユーザの入力に応じてファイルを削除するかどうかを返す。
 func Prompt(path string, seed int64) (bool, error) {
-	prompts := Prompts{
+	pfs := promptFuncs{
 		"yes_no":        promptWithYesNo,
 		"yes_no_jp":     promptWithYesNoInJapanese,
 		"yes_no_denial": promptWithYesNoDenial,
 		"math":          promptWithMath,
 	}
 
-	f := selectFunc(prompts, seed)
+	f := selectFunc(pfs, seed)
 	return f(path)
 }
 
-// selectFunc はpromptsからランダムに1つPromptFuncを返す。
-func selectFunc(prompts Prompts, seed int64) PromptFunc {
+// selectFunc はpfsからランダムに1つpromptFuncを返す。
+func selectFunc(pfs promptFuncs, seed int64) promptFunc {
 	keys := make([]string, 0)
-	for k := range prompts {
+	for k := range pfs {
 		keys = append(keys, k)
 	}
 
 	r := rand.New(rand.NewSource(seed))
 	i := r.Intn(len(keys))
 	k := keys[i]
-	f := prompts[k]
+	f := pfs[k]
 	return f
 }
