1cbc6920d3508b4ad6e5000dac5bb61519f61224
jiro4989 <jiroron666@gmail.com>
2026-01-30 19:49:52 +0900

    fix: operator を domain に切り出し

---
+++ domain/operator.go
@@ -0,0 +1,42 @@
+package domain
+
+type Operator interface {
+	Do(int, int) int
+	Op() string
+}
+
+var (
+	_ Operator = &PlusOperator{}
+	_ Operator = &MinusOperator{}
+	_ Operator = &MultiOperator{}
+)
+
+type PlusOperator struct{}
+
+func (o *PlusOperator) Do(a, b int) int {
+	return a + b
+}
+
+func (o *PlusOperator) Op() string {
+	return "+"
+}
+
+type MinusOperator struct{}
+
+func (o *MinusOperator) Do(a, b int) int {
+	return a - b
+}
+
+func (o *MinusOperator) Op() string {
+	return "-"
+}
+
+type MultiOperator struct{}
+
+func (o *MultiOperator) Do(a, b int) int {
+	return a * b
+}
+
+func (o *MultiOperator) Op() string {
+	return "*"
+}
--- prompts/prompts_math.go
+++ prompts/prompts_math.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/jiro4989/tene3rm/domain"
 	"github.com/manifoldco/promptui"
 )
 
@@ -20,32 +21,21 @@ func promptWithMath(path string) (bool, error) {
 		return nil
 	}
 
-	calcs := map[string]func(int, int) int{
-		"+": func(a, b int) int {
-			return a + b
-		},
-		"-": func(a, b int) int {
-			return a - b
-		},
-		"*": func(a, b int) int {
-			return a * b
-		},
+	ops := []domain.Operator{
+		&domain.PlusOperator{},
+		&domain.MinusOperator{},
+		&domain.MultiOperator{},
 	}
 
 	a := rand.Intn(9) + 1
 	b := rand.Intn(9) + 1
 
-	keys := make([]string, 0)
-	for k := range calcs {
-		keys = append(keys, k)
-	}
-	i := rand.Intn(len(keys))
-	op := keys[i]
-	f := calcs[op]
-	c := f(a, b)
+	i := rand.Intn(len(ops))
+	op := ops[i]
+	c := op.Do(a, b)
 
 	p := promptui.Prompt{
-		Label:    fmt.Sprintf("%s: remove file '%s'? (%d %s %d = ?)", appname, path, a, op, b),
+		Label:    fmt.Sprintf("%s: remove file '%s'? (%d %s %d = ?)", appname, path, a, op.Op(), b),
 		Validate: validate,
 	}
 	result, err := p.Run()
