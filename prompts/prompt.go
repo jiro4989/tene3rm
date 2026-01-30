180b5731e722406aa77b1d384b8f34d8021b0c1b
jiro4989 <jiroron666@gmail.com>
2026-01-30 21:07:35 +0900

    fix: 実装を簡単に変更

--- prompts/prompt.go
+++ prompts/prompt.go
@@ -12,27 +12,15 @@ const appname = "tene3rm"
 
 // Prompt はランダムにプロンプトを表示し、ユーザの入力に応じてファイルを削除するかどうかを返す。
 func Prompt(path string, seed int64) (bool, error) {
-	pfs := promptFuncs{
-		"yes_no":        promptWithYesNo,
-		"yes_no_jp":     promptWithYesNoInJapanese,
-		"yes_no_denial": promptWithYesNoDenial,
-		"math":          promptWithMath,
-	}
-
-	f := selectFunc(pfs, seed)
-	return f(path)
-}
-
-// selectFunc はpfsからランダムに1つpromptFuncを返す。
-func selectFunc(pfs promptFuncs, seed int64) promptFunc {
-	keys := make([]string, 0)
-	for k := range pfs {
-		keys = append(keys, k)
+	pfs := []promptFunc{
+		promptWithYesNo,
+		promptWithYesNoInJapanese,
+		promptWithYesNoDenial,
+		promptWithMath,
 	}
 
 	r := rand.New(rand.NewSource(seed))
-	i := r.Intn(len(keys))
-	k := keys[i]
-	f := pfs[k]
-	return f
+	i := r.Intn(len(pfs))
+	pf := pfs[i]
+	return pf(path)
 }
