16db6a873308040275bbcbb929141ad5c11f9d15
jiro4989 <jiroron666@gmail.com>
2026-01-30 21:16:05 +0900

    fix: 関数で実装を共通化

--- prompts/prompt_yes_no.go
+++ prompts/prompt_yes_no.go
@@ -9,12 +9,26 @@ import (
 
 // promptWithYesNo はシンプルなYes/Noプロンプトを表示する。
 func promptWithYesNo(path string) (bool, error) {
+	return promptWithSimpleText(path, "(*'-')! < remove file '%s'? [y/n]", []string{"y", "ye", "yes"})
+}
+
+// promptWithYesNoDenial はNoのときだけファイルを削除するプロンプトを表示する。
+func promptWithYesNoDenial(path string) (bool, error) {
+	return promptWithSimpleText(path, "(*'-')! < DON't remove file '%s'? [y/n]", []string{"n", "no"})
+}
+
+// promptWithYesNoInJapanese はシンプルなはい/いいえプロンプトを表示する。
+func promptWithYesNoInJapanese(path string) (bool, error) {
+	return promptWithSimpleText(path, "(*'-')! < '%s' ファイルを削除しますか? [はい/いいえ]", []string{"は", "はい"})
+}
+
+func promptWithSimpleText(path string, promptFmt string, wants []string) (bool, error) {
 	validate := func(input string) error {
 		return nil
 	}
 
 	p := promptui.Prompt{
-		Label:    fmt.Sprintf("%s: remove file '%s'? [y/n]", appname, path),
+		Label:    fmt.Sprintf(promptFmt, path),
 		Validate: validate,
 	}
 	result, err := p.Run()
@@ -23,10 +37,13 @@ func promptWithYesNo(path string) (bool, error) {
 	}
 
 	result = strings.TrimSpace(result)
-	switch result {
-	case "y", "ye", "yes":
-		return true, nil
+	found := false
+	for _, want := range wants {
+		if result == want {
+			found = true
+			break
+		}
 	}
 
-	return false, nil
+	return found, nil
 }
