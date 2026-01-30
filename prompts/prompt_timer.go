5021dc06f51f117c03c0283d9e2cd6dfca53cf83
jiro4989 <jiroron666@gmail.com>
2026-01-31 01:19:19 +0900

    fix: 時間判定ミスってたのを修正

--- prompts/prompt_timer.go
+++ prompts/prompt_timer.go
@@ -10,32 +10,41 @@ import (
 
 // promptWithTimer は一定時間経過後に入力を促すプロンプトを表示する。
 func promptWithTimer(_ string) (bool, error) {
-	i := rand.Intn(6) + 4
+	i := rand.Intn(4) + 4
 	msg := fmt.Sprintf("%s < stop in %d seconds [enter]", face, i)
 	fmt.Println(msg)
 
 	now := time.Now()
-
 	running := true
+
+	// タイマーの端末上に表示
 	go printTimer(now, &running)
+
+	// タイマーを手動解除するまでの入力待ち
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
-	running = false
-	aft := time.Now()
 
+	// タイマーをストップする
+	running = false
+
+	// 基準となる時間の前後 1 秒を有効時間とする
+	aft := time.Now()
 	st := aft.Add(-1 * time.Second).Sub(now)
-	x := aft.Sub(now)
 	et := aft.Add(1 * time.Second).Sub(now)
+
+	// 乱数を秒数に変換
+	x := time.Duration(i) * time.Second
 
 	return st <= x && x <= et, nil
 }
 
 func printTimer(t time.Time, running *bool) {
+	const escMove = "\r"
+
 	for *running {
 		duration := time.Now().Sub(t).Seconds()
-		fmt.Println()
 		fmt.Printf("%02.2f seconds", duration)
 		time.Sleep(50 * time.Millisecond)
-		fmt.Print("\x1b[1M")
+		fmt.Print(escMove)
 	}
 }
