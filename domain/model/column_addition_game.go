c0c643389bb6b802c551a78a90dbf2968a1a828c
jiro4989 <jiroron666@gmail.com>
2026-02-05 23:14:12 +0900

    fix: ほとんどの実装をドメインに移動

---
+++ domain/model/column_addition_game.go
@@ -0,0 +1,154 @@
+package model
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type ColumnAdditionGame struct {
+	ca    TwoDigitMultiplyColumnAddition
+	pos   Position
+	cells Cells
+}
+
+func NewColumnAdditionGame(a, b int) (ColumnAdditionGame, error) {
+	ca, err := NewTwoDigitMultiplyColumnAddition(a, b)
+	if err != nil {
+		return ColumnAdditionGame{}, err
+	}
+	return ColumnAdditionGame{
+		ca:    ca,
+		pos:   NewPosition(3, 0, 0, 0, 3, 2),
+		cells: NewCells(),
+	}, nil
+}
+
+func (c ColumnAdditionGame) MoveLeft() ColumnAdditionGame {
+	c.pos = c.pos.MoveLeft()
+	return c
+}
+
+func (c ColumnAdditionGame) MoveRight() ColumnAdditionGame {
+	c.pos = c.pos.MoveRight()
+	return c
+}
+
+func (c ColumnAdditionGame) MoveUp() ColumnAdditionGame {
+	c.pos = c.pos.MoveUp()
+	return c
+}
+
+func (c ColumnAdditionGame) MoveDown() ColumnAdditionGame {
+	c.pos = c.pos.MoveDown()
+	return c
+}
+
+func (c ColumnAdditionGame) SetString(s string) ColumnAdditionGame {
+	cell := NewCell(s)
+	c.cells.value[c.pos.y].value[c.pos.x] = cell
+	return c
+}
+
+func (c ColumnAdditionGame) PositionXY() (int, int) {
+	return c.pos.x, c.pos.y
+}
+
+func (c ColumnAdditionGame) CurrentPositionCellValue() string {
+	return c.cells.value[c.pos.y].value[c.pos.x].value
+}
+
+func (c ColumnAdditionGame) CurrentPositionCellValueRune() rune {
+	return []rune(c.CurrentPositionCellValue())[0]
+}
+
+func (c ColumnAdditionGame) ResultStringLines() []string {
+	result := make([]string, 0)
+	a10, a1 := divMod(c.ca.a)
+	b10, b1 := divMod(c.ca.b)
+	const horizontalLine = "-------"
+	result = append(result, fmt.Sprintf("    %d %d", a10, a1))
+	result = append(result, fmt.Sprintf("  x %d %d", b10, b1))
+	result = append(result, horizontalLine)
+	result = append(result, c.cells.value[0].ResultStringLine())
+	result = append(result, c.cells.value[1].ResultStringLine())
+	result = append(result, horizontalLine)
+	result = append(result, c.cells.value[2].ResultStringLine())
+	return result
+}
+
+func divMod(n int) (int, int) {
+	return n / 10, n % 10
+}
+
+func (c ColumnAdditionGame) Evaluate() (bool, error) {
+	n1, err := c.cells.value[0].ToInt()
+	if err != nil {
+		return false, err
+	}
+	n2, err := c.cells.value[1].ToInt()
+	if err != nil {
+		return false, err
+	}
+	n3, err := c.cells.value[2].ToInt()
+	if err != nil {
+		return false, err
+	}
+	return c.ca.Equal(n1, n2, n3), nil
+}
+
+type Cell struct {
+	value string
+}
+
+func NewCell(s string) Cell {
+	return Cell{value: s}
+}
+
+func NewEmptyCell() Cell {
+	return NewCell(" ")
+}
+
+type Row struct {
+	value []Cell
+}
+
+func NewRow() Row {
+	r := Row{}
+	for i := 0; i < 4; i++ {
+		r.value = append(r.value, NewEmptyCell())
+	}
+	return r
+}
+
+func (r Row) ToStrings() []string {
+	result := make([]string, 0)
+	for _, cell := range r.value {
+		v := cell.value
+		result = append(result, v)
+	}
+	return result
+}
+
+func (r Row) ResultStringLine() string {
+	return strings.Join(r.ToStrings(), " ")
+}
+
+func (r Row) ToInt() (int, error) {
+	// TODO: ここのバリデーションがあまい
+	s := strings.Join(r.ToStrings(), "")
+	s = strings.TrimSpace(s)
+	return strconv.Atoi(s)
+}
+
+type Cells struct {
+	value []Row
+}
+
+func NewCells() Cells {
+	c := Cells{}
+	for i := 0; i < 3; i++ {
+		c.value = append(c.value, NewRow())
+	}
+	return c
+}
--- ui/terminal/prompt_math.go
+++ ui/terminal/prompt_math.go
@@ -3,7 +3,6 @@ package terminal
 import (
 	"fmt"
 	"math/rand"
-	"strings"
 	"time"
 
 	"github.com/jiro4989/tene3rm/domain/model"
@@ -57,23 +56,10 @@ func promptWithMath2(path string) (bool, error) {
 	a := rand.Intn(90) + 10
 	b := rand.Intn(90) + 10
 
-	ca, err := model.NewTwoDigitMultiplyColumnAddition(a, b)
+	cag, err := model.NewColumnAdditionGame(a, b)
 	if err != nil {
 		return false, err
 	}
-
-	var (
-		a10 int = a / 10
-		a1  int = a % 10
-		b10 int = b / 10
-		b1  int = b % 10
-	)
-
-	vals[0][2] = fmt.Sprintf("%d", a10)
-	vals[0][3] = fmt.Sprintf("%d", a1)
-	vals[1][1] = "x"
-	vals[1][2] = fmt.Sprintf("%d", b10)
-	vals[1][3] = fmt.Sprintf("%d", b1)
 
 	if err := termbox.Init(); err != nil {
 		return false, err
@@ -82,9 +68,8 @@ func promptWithMath2(path string) (bool, error) {
 	termbox.SetInputMode(termbox.InputEsc)
 	termbox.Flush()
 
-	pos := model.NewPosition(3, 2, 0, 2, 3, 4)
 	for {
-		drawBackground(pos.X(), pos.Y())
+		drawScreen(cag)
 		time.Sleep(50 * time.Millisecond)
 		switch ev := termbox.PollEvent(); ev.Type {
 		case termbox.EventKey:
@@ -95,96 +80,71 @@ func promptWithMath2(path string) (bool, error) {
 
 			switch ev.Ch {
 			case 'h':
-				pos = pos.MoveLeft()
+				cag = cag.MoveLeft()
 			case 'j':
-				pos = pos.MoveDown()
+				cag = cag.MoveDown()
 			case 'k':
-				pos = pos.MoveUp()
+				cag = cag.MoveUp()
 			case 'l':
-				pos = pos.MoveRight()
+				cag = cag.MoveRight()
 			case '0':
-				setNum(pos, "0")
+				cag = cag.SetString("0")
 			case '1':
-				setNum(pos, "1")
+				cag = cag.SetString("1")
 			case '2':
-				setNum(pos, "2")
+				cag = cag.SetString("2")
 			case '3':
-				setNum(pos, "3")
+				cag = cag.SetString("3")
 			case '4':
-				setNum(pos, "4")
+				cag = cag.SetString("4")
 			case '5':
-				setNum(pos, "5")
+				cag = cag.SetString("5")
 			case '6':
-				setNum(pos, "6")
+				cag = cag.SetString("6")
 			case '7':
-				setNum(pos, "7")
+				cag = cag.SetString("7")
 			case '8':
-				setNum(pos, "8")
+				cag = cag.SetString("8")
 			case '9':
-				setNum(pos, "9")
+				cag = cag.SetString("9")
 			}
 		}
 	}
 fin:
 
-	n1, err := model.NewUserInputText(strings.Join(vals[2], "")).ToInt()
-	if err != nil {
-		return false, err
-	}
-	n2, err := model.NewUserInputText(strings.Join(vals[3], "")).ToInt()
-	if err != nil {
-		return false, err
-	}
-	n3, err := model.NewUserInputText(strings.Join(vals[4], "")).ToInt()
-	if err != nil {
-		return false, err
-	}
-
-	return ca.Equal(n1, n2, n3), nil
+	return cag.Evaluate()
 }
 
 func setNum(pos model.Position, s string) {
 	vals[pos.Y()][pos.X()] = s
 }
 
-func drawBackground(x, y int) {
+func drawScreen(cag model.ColumnAdditionGame) {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 
 	const leftPad = 1
 	const topPad = 1
+	var lastLineY int
+	for y, line := range cag.ResultStringLines() {
+		drawLine(line, leftPad, y+topPad)
+		lastLineY = y
+	}
+	drawLine("h: move left, j: move down, k: move up, l: move right", leftPad, lastLineY+1)
+	drawLine("ENTER: confirm", leftPad, lastLineY+2)
 
-	drawLine(strsToLine(vals[0]), leftPad, 1)
-	drawLine(strsToLine(vals[1]), leftPad, 2)
-	drawLine(horizontalLine, leftPad, 3)
-	drawLine(strsToLine(vals[2]), leftPad, 4)
-	drawLine(strsToLine(vals[3]), leftPad, 5)
-	drawLine(horizontalLine, leftPad, 6)
-	drawLine(strsToLine(vals[4]), leftPad, 7)
-
-	drawLine("h: move left, j: move down, k: move up, l: move right", 1, 9)
-	drawLine("ENTER: confirm", 1, 10)
-
+	x, y := cag.PositionXY()
 	var y2 int
 	if 2 <= y && y < 4 {
 		y2 = y + 1 + topPad
 	} else {
 		y2 = y + 2 + topPad
 	}
-	termbox.SetCell(x*2+leftPad, y2, []rune(vals[y][x])[0], termbox.ColorWhite, termbox.ColorBlack)
+	termbox.SetCell(x*2+leftPad, y2, cag.CurrentPositionCellValueRune(), termbox.ColorWhite, termbox.ColorBlack)
 	termbox.Flush()
 }
 
-const fg = termbox.ColorDefault
-const bg = termbox.ColorDefault
-
 func drawLine(text string, x, y int) {
 	for i, c := range text {
-		termbox.SetCell(x+i, y, c, fg, bg)
+		termbox.SetCell(x+i, y, c, termbox.ColorDefault, termbox.ColorDefault)
 	}
 }
-
-func strsToLine(vals []string) string {
-	return strings.Join(vals, " ")
-}
-
-const horizontalLine = "-------"
