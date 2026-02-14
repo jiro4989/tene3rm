8d3f854a2f93e5dc1f62092afddf238434cb7706
jiro4989 <jiroron666@gmail.com>
2026-02-14 19:46:18 +0900

    add: テストコードを追加

--- domain/column/column_addition_test.go
+++ domain/column/column_addition_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewTwoDigitMultiplyColumnAddition(t *testing.T) {
@@ -63,6 +64,67 @@ func TestNewTwoDigitMultiplyColumnAddition(t *testing.T) {
 				return
 			}
 
+			a.Equal(tt.want, got)
+		})
+	}
+}
+
+func TestTwoDigitMultiplyColumnAddition_MultiplyOnesPlace(t *testing.T) {
+	c, err := NewTwoDigitMultiplyColumnAddition(12, 34)
+	require.NoError(t, err)
+
+	got := c.MultiplyOnesPlace()
+	assert.Equal(t, 48, got)
+}
+
+func TestTwoDigitMultiplyColumnAddition_MultiplyTensPlace(t *testing.T) {
+	c, err := NewTwoDigitMultiplyColumnAddition(12, 34)
+	require.NoError(t, err)
+
+	got := c.MultiplyTensPlace()
+	assert.Equal(t, 36, got)
+}
+
+func TestTwoDigitMultiplyColumnAddition_Multiply(t *testing.T) {
+	c, err := NewTwoDigitMultiplyColumnAddition(12, 34)
+	require.NoError(t, err)
+
+	got := c.Multiply()
+	assert.Equal(t, 408, got)
+}
+
+func TestTwoDigitMultiplyColumnAddition_Equal(t *testing.T) {
+	c, err := NewTwoDigitMultiplyColumnAddition(12, 34)
+	require.NoError(t, err)
+
+	tests := []struct {
+		name       string
+		c          TwoDigitMultiplyColumnAddition
+		n1, n2, n3 int
+		want       bool
+	}{
+		{
+			name: "正常系: 計算結果が一致する",
+			c:    c,
+			n1:   48,
+			n2:   36,
+			n3:   408,
+			want: true,
+		},
+		{
+			name: "正常系: 計算結果が一致しない",
+			c:    c,
+			n1:   48,
+			n2:   36,
+			n3:   409,
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := assert.New(t)
+
+			got := tt.c.Equal(tt.n1, tt.n2, tt.n3)
 			a.Equal(tt.want, got)
 		})
 	}
