fa7d646601d56af23caeac2728529066be957c32
jiro4989 <jiroron666@gmail.com>
2026-02-14 20:24:48 +0900

    fix: コードを分割

--- domain/column/column_addition_game.go
+++ domain/column/column_addition_game.go
@@ -2,8 +2,6 @@ package column
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 
 	"github.com/jiro4989/tene3rm/domain"
 )
@@ -108,60 +106,4 @@ func (c ColumnAdditionGame) Evaluate() (bool, error) {
 		return false, err
 	}
 	return c.ca.Equal(n1, n2, n3), nil
-}
-
-type Cell struct {
-	value string
-}
-
-func NewCell(s string) Cell {
-	return Cell{value: s}
-}
-
-func NewEmptyCell() Cell {
-	return NewCell(" ")
-}
-
-type Row struct {
-	value []Cell
-}
-
-func NewRow() Row {
-	r := Row{}
-	for i := 0; i < 4; i++ {
-		r.value = append(r.value, NewEmptyCell())
-	}
-	return r
-}
-
-func (r Row) ToStrings() []string {
-	result := make([]string, 0)
-	for _, cell := range r.value {
-		v := cell.value
-		result = append(result, v)
-	}
-	return result
-}
-
-func (r Row) ResultStringLine() string {
-	return strings.Join(r.ToStrings(), " ")
-}
-
-func (r Row) ToInt() (int, error) {
-	// TODO: ここのバリデーションがあまい
-	s := strings.Join(r.ToStrings(), "")
-	s = strings.TrimSpace(s)
-	return strconv.Atoi(s)
-}
-
-type Cells struct {
-	value []Row
-}
-
-func NewCells() Cells {
-	c := Cells{}
-	for i := 0; i < 3; i++ {
-		c.value = append(c.value, NewRow())
-	}
-	return c
 }
