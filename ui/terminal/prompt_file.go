801071ffd3bfa912bbd6979a93da0ce82f15e8e4
jiro4989 <jiroron666@gmail.com>
2026-02-16 19:21:50 +0900

    fix: RandomStringFile プロンプト上に削除するファイルを表示

--- ui/terminal/prompt_file.go
+++ ui/terminal/prompt_file.go
@@ -13,7 +13,7 @@ import (
 
 // promptWithRandomCharacterFile はランダムな文字列が書かれたファイルを出力し、
 // そのファイルの内容の入力を求めるプロンプトを表示する。
-func promptWithRandomStringFile(_ string) (bool, error) {
+func promptWithRandomStringFile(path string) (bool, error) {
 	validate := func(input string) error {
 		return nil
 	}
@@ -28,9 +28,11 @@ func promptWithRandomStringFile(_ string) (bool, error) {
 		return false, err
 	}
 
-	path := fr.FullPath(filename)
+	fmt.Println(fmt.Sprintf("%s: remove file '%s'.", appname, path))
+
+	tmpFile := fr.FullPath(filename)
 	p := promptui.Prompt{
-		Label:    fmt.Sprintf("%s: what was written in the '%s' file?", appname, path),
+		Label:    fmt.Sprintf("%s: what was written in the '%s' file?", appname, tmpFile),
 		Validate: validate,
 	}
 	result, err := p.Run()
